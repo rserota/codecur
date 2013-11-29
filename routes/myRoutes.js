31175dd6d3897579ac369ca6f87b5c12de9bcd7a
rserota <raphael.serota@gmail.com>
2013-11-29 00:01:39 -0700

    meh

--- routes/myRoutes.js
+++ routes/myRoutes.js
@@ -1,11 +1,14 @@
 var db = require('../models/mongooseModels.js')
+var bcrypt = require('bcrypt')
+var breakfast = '$2a$08$Qc0hzrLrPz031mDglyBQlOeRnXBdOMN/m1C5g4gNCi1GtaO7x/i3q'
+
 module.exports = {
     index : function(request, response){
         response.redirect('/us')
     },
 
 	home : function(request, response){
-        console.log('how about dem logs?')
+
         db.BlogPost.find()
             .sort({date : -1})
             .limit(1)
@@ -16,13 +19,22 @@ module.exports = {
 	},
 
     submitBlog : function(request, response){
-        var newBlogPost = new db.BlogPost({
-            title : request.body.title,
-            body : request.body.body,
-            tags : ['testing','testing, 1 2']
-        })
-        newBlogPost.save(function(error){
-            response.send('Blog successfully updated')
+        bcrypt.compare(request.body.potato, breakfast, function(error, result){
+            console.log('myroutes 23 result: ', result)
+            if (result === true){
+
+                var newBlogPost = new db.BlogPost({
+                    title : request.body.title,
+                    body : request.body.body,
+                    tags : ['testing','testing, 1 2']
+                })
+                newBlogPost.save(function(error){
+                    response.send('Blog successfully updated')
+                })
+            }
+            else{
+                response.send('nope')
+            }
         })
     },
 
