7af2a88e6992373828d1dbe84730349f2beb41d9
rserota <raphael.serota@gmail.com>
2013-12-06 11:28:12 -0700

    lets try not to break anything

--- public/javascripts/wad.js
+++ public/javascripts/wad.js
@@ -4,11 +4,14 @@ context = new AudioContext();
 var wad = function(arg){
     this.source = arg.source;
     this.volume = arg.volume || 1 // peak volume. min:0, max:1 (actually max is infinite, but ...just keep it at or below 1)
-    this.attack = arg.attack || 0 // time in seconds from onset to peak volume
-    this.decay = arg.decay || 0 // time in seconds from peak volume to sustain volume
-    this.sustain = arg.sustain || 1 // sustain volume level, as a percent of peak volume. min:0, max:1
-    this.hold = arg.hold || 9001 // time in seconds to maintain sustain volume
-    this.release = arg.release || 0 // time in seconds from sustain volume to zero volume
+        this.env = {
+            attack : arg.env ? (arg.env.attack || 0) : 0, // time in seconds from onset to peak volume
+            decay : arg.env ? (arg.env.decay || 0) : 0, // time in seconds from peak volume to sustain volume
+            sustain : arg.env ? (arg.env.sustain || 1) : 1, // sustain volume level, as a percent of peak volume. min:0, max:1
+            hold : arg.env ? (arg.env.hold || 9001) : 9001, // time in seconds to maintain sustain volume
+            release : arg.env ? (arg.env.release || 0) : 0 // time in seconds from sustain volume to zero volume
+        }
+    
     this.setVolume = function(volume){
         this.volume = volume;
         if(this.gain){this.gain.gain.value = volume};
@@ -29,9 +32,9 @@ var wad = function(arg){
                 that.gain.connect(context.destination)
                 //this is the envelope
                 that.gain.gain.linearRampToValueAtTime(0.0001, context.currentTime)
-                that.gain.gain.linearRampToValueAtTime(that.volume, context.currentTime+that.attack)
-                that.gain.gain.linearRampToValueAtTime(that.volume*that.sustain, context.currentTime+that.attack+that.decay)
-                that.gain.gain.linearRampToValueAtTime(0.0001, context.currentTime+that.attack+that.decay+that.hold+that.release)
+                that.gain.gain.linearRampToValueAtTime(that.volume, context.currentTime+that.env.attack)
+                that.gain.gain.linearRampToValueAtTime(that.volume*that.env.sustain, context.currentTime+that.env.attack+that.env.decay)
+                that.gain.gain.linearRampToValueAtTime(0.0001, context.currentTime+that.env.attack+that.env.decay+that.env.hold+that.env.release)
 
                 that.soundSource.start(context.currentTime);
 
@@ -48,28 +51,33 @@ var wad = function(arg){
 
 phone = new wad({
     source : 'http://localhost:3000/us/sendaudio/household022.wav',
-    attack : 1
+    env : {
+        attack : 1
+    }
 })
 
 asm = new wad({
     source : 'http://localhost:3000/us/sendaudio/asm.mp3',
-    attack : 10
 })
 
 tone = new wad({
     source : 'http://localhost:3000/us/sendaudio/A2.wav',
-    attack : .1,
-    decay : .2,
-    sustain : .9,
-    hold : .5,
-    release : .9
+    env : {
+        attack : .1,
+        decay : .2,
+        sustain : .9,
+        hold : .5,
+        release : .9
+    }
 })
 
 tone2 = new wad({
     source : 'http://localhost:3000/us/sendaudio/A2.wav',
-    attack : .1,
-    decay : .2,
-    sustain : .4,
-    hold : .5,
-    release : .9
+    env : {
+        attack : .1,
+        decay : .2,
+        sustain : .4,
+        hold : .5,
+        release : .9
+    }
 })
