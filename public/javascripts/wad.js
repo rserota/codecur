a53aa2489c3769a10b51572e155316afed3ac453
rserota <raphael.serota@gmail.com>
2013-12-06 16:50:43 -0700

    plug em in

--- public/javascripts/wad.js
+++ public/javascripts/wad.js
@@ -4,48 +4,81 @@ context = new AudioContext();
 var wad = function(arg){
     this.source = arg.source;
     this.volume = arg.volume || 1 // peak volume. min:0, max:1 (actually max is infinite, but ...just keep it at or below 1)
-        this.env = {
-            attack : arg.env ? (arg.env.attack || 0) : 0, // time in seconds from onset to peak volume
-            decay : arg.env ? (arg.env.decay || 0) : 0, // time in seconds from peak volume to sustain volume
-            sustain : arg.env ? (arg.env.sustain || 1) : 1, // sustain volume level, as a percent of peak volume. min:0, max:1
-            hold : arg.env ? (arg.env.hold || 9001) : 9001, // time in seconds to maintain sustain volume
-            release : arg.env ? (arg.env.release || 0) : 0 // time in seconds from sustain volume to zero volume
-        }
-    
-    this.setVolume = function(volume){
-        this.volume = volume;
-        if(this.gain){this.gain.gain.value = volume};
+    this.env = {
+        attack : arg.env ? (arg.env.attack || 0) : 0, // time in seconds from onset to peak volume
+        decay : arg.env ? (arg.env.decay || 0) : 0, // time in seconds from peak volume to sustain volume
+        sustain : arg.env ? (arg.env.sustain || 1) : 1, // sustain volume level, as a percent of peak volume. min:0, max:1
+        hold : arg.env ? (arg.env.hold || 9001) : 9001, // time in seconds to maintain sustain volume
+        release : arg.env ? (arg.env.release || 0) : 0 // time in seconds from sustain volume to zero volume
     }
+    
 
-    this.playSound = function(arg){
-        if(arg && arg.volume){this.volume = arg.volume}
+    if(!(this.source in {'sine':0, 'sawtooth':0, 'square':0, 'triangle':0})){
+        /** fetch resources **/
         var request = new XMLHttpRequest();
         request.open("GET", this.source, true);
         request.responseType = "arraybuffer";
         var that = this
         request.onload = function() {
             context.decodeAudioData(request.response, function onSuccess(decodedBuffer){
-                that.soundSource = context.createBufferSource();
-                that.soundSource.buffer = decodedBuffer;    
-                that.gain = context.createGain()
-                that.soundSource.connect(that.gain);
-                that.gain.connect(context.destination)
-                //this is the envelope
-                that.gain.gain.linearRampToValueAtTime(0.0001, context.currentTime)
-                that.gain.gain.linearRampToValueAtTime(that.volume, context.currentTime+that.env.attack)
-                that.gain.gain.linearRampToValueAtTime(that.volume*that.env.sustain, context.currentTime+that.env.attack+that.env.decay)
-                that.gain.gain.linearRampToValueAtTime(0.0001, context.currentTime+that.env.attack+that.env.decay+that.env.hold+that.env.release)
-
-                that.soundSource.start(context.currentTime);
-
-            })        
-        };
+                that.decodedBuffer = decodedBuffer
+            })
+        }
         request.send();
+        //////////////////////
+    }
+
+    this.setVolume = function(volume){
+        this.volume = volume;
+        // if(this.gain){this.gain.gain.value = volume};
+    }
+
+    this.playEnv = function(){
+        this.gain.gain.linearRampToValueAtTime(0.0001, context.currentTime)
+        this.gain.gain.linearRampToValueAtTime(this.volume, context.currentTime+this.env.attack)
+        this.gain.gain.linearRampToValueAtTime(this.volume*this.env.sustain, context.currentTime+this.env.attack+this.env.decay)
+        this.gain.gain.linearRampToValueAtTime(0.0001, context.currentTime+this.env.attack+this.env.decay+this.env.hold+this.env.release)
+        this.soundSource.stop(context.currentTime+this.env.attack+this.env.decay+this.env.hold+this.env.release)
+        ///////////////////////////
+        this.soundSource.start(context.currentTime);
+    }
+
+    this.nodes = []
+    this.plugEmIn = function(){
+        for (var i=1; i<this.nodes.length; i++){
+            this.nodes[i-1].connect(this.nodes[i])
+        }
+    }
+
+    this.play = function(arg){
+        if(arg && arg.volume){this.volume = arg.volume}
+        if(this.source in {'sine':0, 'sawtooth':0, 'square':0, 'triangle':0}){            
+            this.soundSource = context.createOscillator()
+            this.soundSource.type = this.source
+        }
+        else{
+            this.soundSource = context.createBufferSource();
+            this.soundSource.buffer = that.decodedBuffer;    
+        }
+
+        this.nodes = []
+        this.nodes.push(this.soundSource)
+
+        this.gain = context.createGain()
+        this.nodes.push(this.gain)
+
+        this.nodes.push(context.destination)
+
+        this.plugEmIn()
+
+        /** this is the envelope **/
+        this.playEnv()
     }
 
 //If multiple instances of a sound are playing simultaneously, stopSound only can stop the most recent one
-    this.stopSound = function(){
-        this.soundSource.stop(0)
+    this.stop = function(){
+        this.gain.gain.linearRampToValueAtTime(.0001, context.currentTime+this.env.release)
+        this.soundSource.stop(context.currentTime+this.env.release)
     }
 }
 
@@ -58,6 +91,9 @@ phone = new wad({
 
 asm = new wad({
     source : 'http://localhost:3000/us/sendaudio/asm.mp3',
+    env : {
+        release : 5
+    }
 })
 
 tone = new wad({
@@ -71,13 +107,25 @@ tone = new wad({
     }
 })
 
-tone2 = new wad({
-    source : 'http://localhost:3000/us/sendaudio/A2.wav',
+
+saw = new wad({
+    source : 'sawtooth',
     env : {
-        attack : .1,
+        attack : .5,
+        decay : .5,
+        sutain : .9,
+        hold : 2,
+        release : 1
+    }
+})
+
+tri = new wad({
+    source : 'triangle',
+    env : {
+        attack : .15,
         decay : .2,
-        sustain : .4,
-        hold : .5,
-        release : .9
+        sutain : .9,
+        hold : 2,
+        release : 1
     }
 })
