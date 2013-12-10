2782e52131e3a79e8b75ef82f4fccdca75b2521a
rserota <raphael.serota@gmail.com>
2013-12-10 15:16:52 -0700

    fixed filters

--- public/javascripts/audiotest.js
+++ public/javascripts/audiotest.js
@@ -1,40 +1,5 @@
-
-    var context, 
-        audioData,
-        soundSource, 
-        decodedBuffer,
-        url = 'http://localhost:3000/us/sendaudio';
-
-    // Step 1 - Initialise the Audio Context
-    function init() {
-        if (typeof AudioContext !== "undefined") {
-            context = new AudioContext();
-        } else if (typeof webkitAudioContext !== "undefined") {
-            context = new webkitAudioContext();
-        } else {
-            throw new Error('AudioContext not supported. :(');
-        }
-    }
-
-    // Step 2: Load our Sound using XHR
-    function playSound() {
-        var request = new XMLHttpRequest();
-        request.open("GET", url, true);
-        request.responseType = "arraybuffer";
-        request.onload = function() {
-            context.decodeAudioData(request.response, function onSuccess(decodedBuffer){
-                decodedBuffer = decodedBuffer
-                soundSource = context.createBufferSource();
-                soundSource.buffer = decodedBuffer;    
-                soundSource.connect(context.destination);
-                soundSource.start(0);
-
-            })        
-        };
-        request.send();
-    }
-
-
-    init();
-    playSound() 
-
+$(document).ready(function(){
+    $('.playbutton').on('click', function(){
+        saw.play({pitch:$(this).text()})
+    })
+})
