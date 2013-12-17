78dc32031053b20012327e51e1638e5dbee816d3
rserota <raphael.serota@gmail.com>
2013-12-17 10:07:09 -0700

    less padding

--- public/javascripts/audiotest.js
+++ public/javascripts/audiotest.js
@@ -19,12 +19,12 @@ var checkValidName = function(){
 }
 $(document).ready(function(){
 
-    $(document).on('click', 'ul.dropdown-menu li',function(){
-        var $this = $(this)
-        $this.siblings().removeClass('active')
-        $this.addClass('active')
-        $this.closest('ul').siblings('.relabel').text($this.children('a').text())
-    })
+    // $(document).on('click', 'ul.dropdown-menu li',function(){
+    //     var $this = $(this)
+    //     $this.siblings().removeClass('active')
+    //     $this.addClass('active')
+    //     $this.closest('ul').siblings('.relabel').text($this.children('a').text())
+    // })
     $(document).on('tap', 'ul.dropdown-menu li',function(){
         var $this = $(this)
         $this.siblings().removeClass('active')
@@ -32,23 +32,23 @@ $(document).ready(function(){
         $this.closest('ul').siblings('.relabel').text($this.children('a').text())
     })
 
-    $('#wadmakerbutton').on('click', function(){
-        var wadname = $('#wadname').val()
-        var template = _.template($('#wadtemplate').html())
-        $('.wads').append(template({name : wadname}))
+    // $('#wadmakerbutton').on('click', function(){
+    //     var wadname = $('#wadname').val()
+    //     var template = _.template($('#wadtemplate').html())
+    //     $('.wads').append(template({name : wadname}))
 
-        newWad.source = $('#source').text().toLowerCase()
-        if($('#reverb').text() === 'None'){newWad.reverb.wet = 0.00001}
-        if($('#reverb').text() === 'Quiet'){newWad.reverb.wet = .4}
-        if($('#reverb').text() === 'Loud'){newWad.reverb.wet = 1}
-        if($('#attack').text() === 'Fast'){newWad.env.attack = .1}
-        if($('#attack').text() === 'Slow'){newWad.env.attack = .5}
-        if($('#release').text() === 'Fast'){newWad.env.release = .2}
-        if($('#release').text() === 'Slow'){newWad.env.release = .5}
-        console.log(newWad)
-        wads[wadname] = new Wad(newWad)
-        checkValidName()
-    })
+    //     newWad.source = $('#source').text().toLowerCase()
+    //     if($('#reverb').text() === 'None'){newWad.reverb.wet = 0.00001}
+    //     if($('#reverb').text() === 'Quiet'){newWad.reverb.wet = .4}
+    //     if($('#reverb').text() === 'Loud'){newWad.reverb.wet = 1}
+    //     if($('#attack').text() === 'Fast'){newWad.env.attack = .1}
+    //     if($('#attack').text() === 'Slow'){newWad.env.attack = .5}
+    //     if($('#release').text() === 'Fast'){newWad.env.release = .2}
+    //     if($('#release').text() === 'Slow'){newWad.env.release = .5}
+    //     console.log(newWad)
+    //     wads[wadname] = new Wad(newWad)
+    //     checkValidName()
+    // })
 
     $('#wadmakerbutton').on('tap', function(){
         var wadname = $('#wadname').val()
@@ -68,11 +68,11 @@ $(document).ready(function(){
         checkValidName()
     })
 
-    $(document).on('click', '.glyphicon-remove-circle', function(){
-        wads[$(this).siblings('.h1').text()] = null
-        $(this).closest('.wad').remove()
-        checkValidName()
-    })
+    // $(document).on('click', '.glyphicon-remove-circle', function(){
+    //     wads[$(this).siblings('.h1').text()] = null
+    //     $(this).closest('.wad').remove()
+    //     checkValidName()
+    // })
 
     $(document).on('tap', '.glyphicon-remove-circle', function(){
         wads[$(this).siblings('.h1').text()] = null
@@ -80,14 +80,14 @@ $(document).ready(function(){
         checkValidName()
     })
 
-    $(document).on('click', '.playbutton', function(){
-        console.log($(this).siblings('.notename').text())
-        var notename = $(this).siblings('.notename').text() + '4'
-        var wadname = $(this).closest('.wad').find('.h1').text()
-        console.log('notename: ',notename)
-        console.log('wadname: ',wadname)
-        wads[wadname].play({pitch : notename})
-    })
+    // $(document).on('click', '.playbutton', function(){
+    //     console.log($(this).siblings('.notename').text())
+    //     var notename = $(this).siblings('.notename').text() + '4'
+    //     var wadname = $(this).closest('.wad').find('.h1').text()
+    //     console.log('notename: ',notename)
+    //     console.log('wadname: ',wadname)
+    //     wads[wadname].play({pitch : notename})
+    // })
     $(document).on('tap', '.playbutton', function(){
         console.log($(this).siblings('.notename').text())
         var notename = $(this).siblings('.notename').text() + '4'
