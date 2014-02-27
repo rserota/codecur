d8a043663ddb0289a8d1ec32b58336ffb1f0479e
rserota <raphael.serota@gmail.com>
2014-02-27 09:17:14 -0700

    ...

--- public/javascripts/waddemo.js
+++ public/javascripts/waddemo.js
@@ -1,86 +1,86 @@
-var bloop = new Wad({
-    source : 'sine',
+// var bloop = new Wad({
+//     source : 'sine',
    
-    env : {
-        attack : .1,
-        decay : .3,
-        hold : .3,
-        sustain : .8,
-        release : .2
-    },
-    panning : 0,
-    reverb : .3
+//     env : {
+//         attack : .1,
+//         decay : .3,
+//         hold : .3,
+//         sustain : .8,
+//         release : .2
+//     },
+//     panning : 0,
+//     reverb : .3
 
 
-})
+// })
 
-// bloop.play()
+// // bloop.play()
 
-// var stereoBounce = setInterval(function(){
-//     bloop.play({panning : 11})
-//     setTimeout(function(){
-//         bloop.play({panning : -11})
-//     }, 500)
-// }, 1000)
+// // var stereoBounce = setInterval(function(){
+// //     bloop.play({panning : 11})
+// //     setTimeout(function(){
+// //         bloop.play({panning : -11})
+// //     }, 500)
+// // }, 1000)
 
-var noise = new Wad({
-    source : 'noise',
-    volume : 2,
-    filter : {
-        type : 'lowpass',
-        frequency : 900,
-        q : .9
-    },
-    // reverb : .2,
-    env : {
-        attack : .02,
-        decay : .01,
-        hold : .1,
-        sustain : .1,
-        release : .01
-    }
-})
-
-// var mic = new Wad({
-//     source : 'mic',
-//     volume : 2.0,
-//     reverb : .2,
+// var noise = new Wad({
+//     source : 'noise',
+//     volume : 2,
 //     filter : {
 //         type : 'lowpass',
-//         frequency : 200
+//         frequency : 900,
+//         q : .9
+//     },
+//     // reverb : .2,
+//     env : {
+//         attack : .02,
+//         decay : .01,
+//         hold : .1,
+//         sustain : .1,
+//         release : .01
 //     }
 // })
 
-var bpm = 80
-var pattern = [
-    {instrument : noise, args : {}, beat : 1.5},
-    {instrument : noise, args : {}, beat : 2.5},
-    {instrument : noise, args : {}, beat : 3.5},
-    {instrument : noise, args : {}, beat : 4.5},
-    {instrument : noise, args : {}, beat : 5.5},
-    {instrument : noise, args : {}, beat : 6.5},
-    {instrument : noise, args : {}, beat : 7.5},
-    {instrument : noise, args : {}, beat : 8.5},
-    {instrument : bloop, args : {pitch: 'C5'}, beat : 1},
-    {instrument : bloop, args: {pitch: 'Eb5'}, beat : 3},
-        {instrument : bloop, args : {pitch: 'C5'}, beat : 1.5},
+// // var mic = new Wad({
+// //     source : 'mic',
+// //     volume : 2.0,
+// //     reverb : .2,
+// //     filter : {
+// //         type : 'lowpass',
+// //         frequency : 200
+// //     }
+// // })
 
-    {instrument : bloop, args: {pitch: 'G5'}, beat : 5},
-    {instrument : bloop, args: {pitch: 'Bb5'}, beat : 7}
-]
+// var bpm = 80
+// var pattern = [
+//     {instrument : noise, args : {}, beat : 1.5},
+//     {instrument : noise, args : {}, beat : 2.5},
+//     {instrument : noise, args : {}, beat : 3.5},
+//     {instrument : noise, args : {}, beat : 4.5},
+//     {instrument : noise, args : {}, beat : 5.5},
+//     {instrument : noise, args : {}, beat : 6.5},
+//     {instrument : noise, args : {}, beat : 7.5},
+//     {instrument : noise, args : {}, beat : 8.5},
+//     {instrument : bloop, args : {pitch: 'C5'}, beat : 1},
+//     {instrument : bloop, args: {pitch: 'Eb5'}, beat : 3},
+//         {instrument : bloop, args : {pitch: 'C5'}, beat : 1.5},
 
-var playTimeout = function(pattern, i){
-    setTimeout(function(){pattern[i].instrument.play(pattern[i].args)}, (pattern[i].beat - 1) * (60000/bpm))
-}
+//     {instrument : bloop, args: {pitch: 'G5'}, beat : 5},
+//     {instrument : bloop, args: {pitch: 'Bb5'}, beat : 7}
+// ]
 
-var playPattern = function(pattern){
-    for (var i = 0; i < pattern.length; i++){
-        playTimeout(pattern, i)
-    } 
-}
+// var playTimeout = function(pattern, i){
+//     setTimeout(function(){pattern[i].instrument.play(pattern[i].args)}, (pattern[i].beat - 1) * (60000/bpm))
+// }
 
-setInterval(function(){
-    playPattern(pattern)
-}, 8000)
+// var playPattern = function(pattern){
+//     for (var i = 0; i < pattern.length; i++){
+//         playTimeout(pattern, i)
+//     } 
+// }
+
+// setInterval(function(){
+//     playPattern(pattern)
+// }, 8000)
 
 
