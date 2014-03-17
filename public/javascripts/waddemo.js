6259e990c2430f672c449be963732938a7361c38
rserota <raphael.serota@gmail.com>
2014-03-16 20:24:06 -0600

    ...

--- public/javascripts/waddemo.js
+++ public/javascripts/waddemo.js
@@ -17,24 +17,64 @@ var bass = new Wad({
 
 var oneLoop = function() {
 
-    setTimeout(function(){ kick.play() }, 0000)
-    setTimeout(function(){ kick.play() }, 1000)
-    setTimeout(function(){ kick.play() }, 2000)
-    setTimeout(function(){ kick.play() }, 3000)
-    setTimeout(function(){ kick.play() }, 4000)
-    setTimeout(function(){ kick.play() }, 5000)
-    setTimeout(function(){ kick.play() }, 6000)
-    setTimeout(function(){ kick.play() }, 7000)
+    setTimeout(function(){ kick.play() }, 00000)
+    setTimeout(function(){ kick.play() }, 01000)
+    setTimeout(function(){ kick.play() }, 02000)
+    setTimeout(function(){ kick.play() }, 03000)
+    setTimeout(function(){ kick.play() }, 04000)
+    setTimeout(function(){ kick.play() }, 05000)
+    setTimeout(function(){ kick.play() }, 06000)
+    setTimeout(function(){ kick.play() }, 07000)
 
-    // setTimeout(function(){ bass.play({pitch : 'C2'}) }, 0000)
+    setTimeout(function(){ kick.play() }, 08000)
+    setTimeout(function(){ kick.play() }, 09000)
+    setTimeout(function(){ kick.play() }, 10000)
+    setTimeout(function(){ kick.play() }, 11000)
+    setTimeout(function(){ kick.play() }, 12000)
+    setTimeout(function(){ kick.play() }, 13000)
+    setTimeout(function(){ kick.play() }, 14000)
+    setTimeout(function(){ kick.play() }, 15000)
 
+    setTimeout(function(){ bass.play({ pitch : 'C2' }) }, 00000)
+    setTimeout(function(){ bass.play({ pitch : 'C3' }) }, 00500)
+    setTimeout(function(){ bass.play({ pitch : 'C2' }) }, 01000)
+    setTimeout(function(){ bass.play({ pitch : 'C3' }) }, 01500)
+    setTimeout(function(){ bass.play({ pitch : 'C2' }) }, 02000)
+    setTimeout(function(){ bass.play({ pitch : 'C3' }) }, 02500)
+    setTimeout(function(){ bass.play({ pitch : 'C2' }) }, 03000)
+    setTimeout(function(){ bass.play({ pitch : 'C3' }) }, 03500)
+    setTimeout(function(){ bass.play({ pitch : 'F2' }) }, 04000)
+    setTimeout(function(){ bass.play({ pitch : 'F3' }) }, 04500)
+    setTimeout(function(){ bass.play({ pitch : 'F2' }) }, 05000)
+    setTimeout(function(){ bass.play({ pitch : 'F3' }) }, 05500)
+    setTimeout(function(){ bass.play({ pitch : 'F2' }) }, 06000)
+    setTimeout(function(){ bass.play({ pitch : 'F3' }) }, 06500)
+    setTimeout(function(){ bass.play({ pitch : 'F2' }) }, 07000)
+    setTimeout(function(){ bass.play({ pitch : 'F3' }) }, 07500)
+
+    setTimeout(function(){ bass.play({ pitch : 'Ab2'}) }, 08000)
+    setTimeout(function(){ bass.play({ pitch : 'Ab3'}) }, 08500)
+    setTimeout(function(){ bass.play({ pitch : 'Ab2'}) }, 09000)
+    setTimeout(function(){ bass.play({ pitch : 'Ab3'}) }, 09500)
+    setTimeout(function(){ bass.play({ pitch : 'Ab2'}) }, 10000)
+    setTimeout(function(){ bass.play({ pitch : 'Ab3'}) }, 10500)
+    setTimeout(function(){ bass.play({ pitch : 'Ab2'}) }, 11000)
+    setTimeout(function(){ bass.play({ pitch : 'Ab3'}) }, 11500)
+    setTimeout(function(){ bass.play({ pitch : 'G2' }) }, 12000)
+    setTimeout(function(){ bass.play({ pitch : 'G3' }) }, 12500)
+    setTimeout(function(){ bass.play({ pitch : 'G2' }) }, 13000)
+    setTimeout(function(){ bass.play({ pitch : 'G3' }) }, 13500)
+    setTimeout(function(){ bass.play({ pitch : 'G2' }) }, 14000)
+    setTimeout(function(){ bass.play({ pitch : 'G3' }) }, 14500)
+    setTimeout(function(){ bass.play({ pitch : 'G2' }) }, 15000)
+    setTimeout(function(){ bass.play({ pitch : 'Bb2'}) }, 15500)
 
 }
 
 $(document).ready(function(){
     $('#go').on('click', function(){
         oneLoop();
-        setInterval(oneLoop, 8000);
+        setInterval(oneLoop, 16000);
     })
     
 })
